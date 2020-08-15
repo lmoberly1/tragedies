d6348773dd29edcd1f28cf0545f28bdfd437102e
lmoberly1 <lukemoberly@gmail.com>
2020-08-15 01:06:42 -0500

    added initial text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var timesRun = 0;
 var divNumber = 1;
 
-function test() {
+function start() {
     setTimeout(function() {
         var x = document.getElementsByClassName("name");
         for (i=0; i<x.length; i++) {
@@ -10,11 +10,9 @@ function test() {
         };
     }, 0)
 
-};
+}
 
-test();
-
-function start(casualties, divID, nameID, speed) {
+function animation(casualties, divID, nameID, speed) {
 
     var x = document.getElementsByClassName("name");
     for (i=0; i<x.length; i++) {
@@ -35,19 +33,19 @@ function start(casualties, divID, nameID, speed) {
 
             switch(divNumber) {
                 case 2:
-                    start(100, "katrina-div", "katrina-name", 10);
+                    animation(100, "katrina-div", "katrina-name", 10);
                     break;
                 case 3:
-                    start(100, "harbor-div", "harbor-name", 10);
+                    animation(100, "harbor-div", "harbor-name", 10);
                     break;
                 case 4:
-                    start(100, "towers-div", "towers-name", 10);
+                    animation(100, "towers-div", "towers-name", 10);
                     break;
                 case 5:
-                    start(100, "iraq-div", "iraq-name", 10);
+                    animation(100, "iraq-div", "iraq-name", 10);
                     break;
                 case 6:
-                    start(100, "covid-div", "covid-name", 10);
+                    animation(100, "covid-div", "covid-name", 10);
                     break;
                 case 7:
                     // text();
@@ -66,8 +64,7 @@ function start(casualties, divID, nameID, speed) {
             userDiv.appendChild(newcontent.firstChild);
         }
     }, speed)
-
-};
+}
 
 function text() {
     setTimeout(function() {
@@ -76,10 +73,34 @@ function text() {
             x[i].className += " hidden"
         };
     }, 3000)
-
 }
 
-setTimeout(function() {
-    start(16, "okc-div", "okc-name", 250);
-}, 500)
+// Text Animation
+function beginningText() {
+
+    document.getElementsByClassName("grid-div").className += " hidden";
+    document.getElementsByClassName("title").className += " opaque";
+    document.getElementsByClassName("begText").className = "begText";
+
+    $(function () {
+        count = 0;
+        wordsArray = ["Moms", "Dads", "Sons", "Daughters", "Grandsons", "Granddaughters", "Uncles", "Aunts", "Friends", "Teachers", "Nurses", "Paramedics", "Mechanics", "Farmers", "Neighbors", "Americans"];
+        var intervalID = setInterval(function () {
+            count++;
+            $("#word").fadeOut(100, function () {
+                $(this).text(wordsArray[count % wordsArray.length]).fadeIn(100);
+            });
+            if(count >= wordsArray.length - 1) {
+                clearInterval(intervalID);
+                start();
+                setTimeout(function() {
+                    animation(16, "okc-div", "okc-name", 250);
+                }, 500)
+            }
+        }, 500);
+    });
+}
+
+beginningText();
+
 
