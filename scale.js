d97fc75957f55b9142452c44c73d43d16f52d52e
lmoberly1 <lukemoberly@gmail.com>
2020-08-17 21:15:54 -0500

    added auto scroll during zoom animation

--- scale.js
+++ scale.js
@@ -14,8 +14,8 @@ function scaleDiv(div, proportional) {
       scaleY = scaleX;
     }
   
-    var translationX = Math.round((availableWidth - (currentWidth * scaleX)) / 2);
-    var translationY = Math.round((availableHeight - (currentHeight * scaleY)) / 2);
+    // var translationX = Math.round((availableWidth - (currentWidth * scaleX)) / 2);
+    // var translationY = Math.round((availableHeight - (currentHeight * scaleY)) / 2);
 
     div.css({
         "-webkit-transform": "scale(" + scaleX + ", " + scaleY + ")",
@@ -25,5 +25,18 @@ function scaleDiv(div, proportional) {
         "transform": "scale(" + scaleX + ", " + scaleY + ")",
     });
 
+    // $('#body').css({
+    //     'width': '100%',
+    //     'height': '100%',
+    // });
+
+    var count = 0;
+    var scrollTransform = setInterval(function() {
+        count++;
+        window.scrollBy(0, -currentHeight/1000);
+        if(count >= 1000) {
+            clearInterval(scrollTransform);
+        }
+    }, 10);
 
   }
